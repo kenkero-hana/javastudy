17c75cf94fb646e005a3f2570070ab58c00b46b3
kenkero-hana <watanabe.kenkanfu@gmail.com>
2023-08-24 16:04:56 +0900

    0824_解決

--- prob1_3.java
+++ prob1_3.java
@@ -1,17 +1,27 @@
 public class prob1_3{
     public static void main(String args[]){
-        probclass1_3 s1, s2;
         int a = (int)(Math.random() * 10) + 1;
         int b = (int)(Math.random() * 10) + 1;
 
         System.out.println("たて：" + a);
         System.out.println("よこ：" + b);
 
-        s1 = new probclass1_3(a,b);
+        square(a, b);
+    }
+    static int square(int a, int b){
+        int i;
+        int j;
 
-        if(s1 == 1 ){
-            System.out.println("■")；
+        for(i = 0; i < a; i++){
+            for(j = 0; j < b; j++){
+                if(i < 0 && j < 0){
+                    System.out.println("");
+                }else{
+                    System.out.print("■ ");
+                }
+            }
+            System.out.println();
         }
-        //square(a, b);
+        return 0;
     }
 }
