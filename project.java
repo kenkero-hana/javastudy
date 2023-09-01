81f7e974464cced138a8a97ba63b8f279ace1f3a
kenkero-hana <watanabe.kenkanfu@gmail.com>
2023-09-01 16:43:10 +0900

    0901

--- project.java
+++ project.java
@@ -2,13 +2,26 @@ import java.util.*;
 
 public class project{
     public static void main(String args[]){
-        ArrayList<Tenram> al = new ArrayList<Tenram>();
-
-        al.add(new Tenram());
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        ArrayList<Integer> bl = new ArrayList<Integer>();
+        //al.add(rand1);
         
-        for(Integer rm : al){
-            System.out.println("0〜10の値を出力：" + );
+        for(int i = 0; ;i++){
+            int a = (int)(Math.random()* 10);
+            System.out.println("0〜10の値を出力：" + a);
+            if(a == 0){
+                break;
+            }
+
+            if(a % 2 == 0){
+                al.add(a);
+            }else{
+                bl.add(a);
+            }
         }
 
+        for(int i = 0; ; i++){
+            
+        }
     }
 }
